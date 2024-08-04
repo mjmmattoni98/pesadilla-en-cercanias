d7a6b897ae20ce697b3638f5c98c16ad154bde01
Marcos Javier Magni Mattoni <javier.magni@gmail.com>
2024-08-04 19:06:33 +0200

    connected drizzle and turso

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,9 +4,12 @@ import { env } from "~/env";
 
 export default {
   schema: "./src/server/db/schema.ts",
+  out: './migrations',
   dialect: "sqlite",
+  driver: 'turso',
   dbCredentials: {
     url: env.DATABASE_URL,
+    authToken: env.DATABASE_AUTH_TOKEN,
   },
   tablesFilter: ["pesadilla-en-cercanias_*"],
 } satisfies Config;
